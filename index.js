161931b6cb69df56ac401c24a4974857b1fd7c26
Gabriel Bueno <gabrielsbueno@hotmail.com>
2022-01-20 00:51:45 -0300

    Adiciona css básico

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
 const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
@@ -44,8 +46,10 @@ io.on('connection', (socket) => {
   });
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + 'index.html');
 });
 
 http.listen(3000, () => {
