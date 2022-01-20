11598632ddf301a8330ed3bbed30301003c779bf
Gabriel Bueno <gabrielsbueno@hotmail.com>
2022-01-19 23:25:23 -0300

    Exercício par ou ímpar

---
+++ Readme.md
@@ -0,0 +1,12 @@
+Tecnologias utilizadas no projeto:
+
+- Socket.io - Responsável pela criação do servidor p/ estabelecer a comunicação baseada em eventos, em real-time.
+
+Para inciar a aplicação:
+
+- Faça o download do repositório
+- Use o script npm start no seu terminal
+- Acesse sua aplicação através do localhost:3000
+- Cada acesso através do browser irá gerar uma conexão (simulando vários jogadores entrando na aplicação)
+- Defina as jogadas de cada jogador
+- Quando todos os jogadores enviarem suas jogadas, os resultados serão exibidos individualmente para cada um
---
+++ index.js
@@ -0,0 +1,53 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000'
+  }
+});
+
+let players = [];
+let plays = 0;
+
+io.on('connection', (socket) => {
+  const oddOrEven = Math.round(Math.random() * 2);
+  let team = '';
+
+  if (oddOrEven === 1) {
+    team = 'ÍMPAR';
+  } else {
+    team = 'PAR';
+  }
+
+  players.push({ id: socket.id, team });
+
+  io.emit('updatePlayers', players.map(({ id }) => id));
+
+  socket.emit('updateTeam', team);
+
+  socket.on('play', (value) => {
+    plays += 1;
+    const playerIndex = players.findIndex(({ id }) => id === socket.id);
+    players[playerIndex].value = +value;
+    const sum = players.map(({ value }) => value).reduce((acc, cur) => acc + cur, 0);
+    const result = sum % 2 === 0 ? 'PAR' : 'ÍMPAR';
+    if (plays === players.length) {
+      io.emit('winners', { sum, result });
+      plays = 0;
+    }
+  });
+
+  socket.on('disconnect', () => {
+    players = players.filter(({ id }) => id !== socket.id);
+    io.emit('updatePlayers', players.map(({ id }) => id));
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000')
+});
